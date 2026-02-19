97af51fc9f36119feb725a00b37544fb0bfc65e4
MachaRaghavi <raghavimacha14@gmail.com>
2026-02-19 08:05:18 +0530

    Add files via upload

---
+++ factors-in-range.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+int num;
+int r1;
+int r2;
+printf("Enter a Number:\n");
+scanf("%d",&num);
+printf("Enter the start point of range:\n");
+scanf("%d",&r1);
+printf("Enter the stop point of range:\n");
+scanf("%d",&r2);
+int count=0;
+for(int i=r1;i<=r2;i++)
+{
+if(num%i==0)
+{count++;
+}}
+printf("count=%d",count);
+return 0;
+}
+
+    
